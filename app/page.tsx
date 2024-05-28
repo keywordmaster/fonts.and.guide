b2dcf5fb7cfb009bf1bd33fc4d1ad33579d33983
imjlk <i@m.junglei.kim>
2024-05-29 03:45:47 +0900

    fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+export const runtime = "edge";
+
 import { gql } from "@urql/core";
 import Link from "next/link";
 
@@ -33,10 +35,10 @@ export default async function Home() {
       <ul>
         {data
           ? data.posts.nodes.map((node) => (
-              <Link key={node.id} href={node.uri}>
-                <li key={node.id}>{node.title}</li>
-              </Link>
-            ))
+            <Link key={node.id} href={node.uri}>
+              <li key={node.id}>{node.title}</li>
+            </Link>
+          ))
           : JSON.stringify(error)}
       </ul>
     </main>
