79ea6233ea170fa97f6a6d357c4093797c11a911
imjlk <i@m.junglei.kim>
2024-05-29 21:46:34 +0900

    fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,46 +36,37 @@ export default async function RootLayout({
     }
   `;
 
-  const { data, error } = await getClient().query<GetRootLayoutQuery>(
-    RootLayoutQuery,
-    {},
-  );
+  // const { data, error } = await getClient().query<GetRootLayoutQuery>(
+  //   RootLayoutQuery,
+  //   {},
+  // );
 
   return (
     <html lang="ko">
-      {data ? (
-        <>
-          <head>
-            <title>{data?.generalSettings?.title}</title>
-            <meta
-              name="description"
-              content={data?.generalSettings?.description}
-            />
-          </head>
-          <body>
-            <header>
-              <div>
-                <h1>
-                  <Link href="/">{data?.generalSettings?.title}</Link>
-                </h1>
+      <head>
+        {/* <title>{data?.generalSettings?.title}</title>
+        <meta name="description" content={data?.generalSettings?.description} /> */}
+      </head>
+      <body>
+        <header>
+          <div>
+            <h1>
+              {/* <Link href="/">{data?.generalSettings?.title}</Link> */}
+            </h1>
 
-                <h5>{data?.generalSettings?.description}</h5>
-              </div>
+            {/* <h5>{data?.generalSettings?.description}</h5> */}
+          </div>
 
-              <ul>
-                {data?.primaryMenuItems?.nodes.map((node) => (
-                  <li key={node.id}>
-                    <Link href={node.uri}>{node.label}</Link>
-                  </li>
-                ))}
-              </ul>
-            </header>
-            {children}
-          </body>
-        </>
-      ) : (
-        JSON.stringify(error)
-      )}
+          <ul>
+            {/* {data?.primaryMenuItems?.nodes.map((node) => (
+              <li key={node.id}>
+                <Link href={node.uri}>{node.label}</Link>
+              </li>
+            ))} */}
+          </ul>
+        </header>
+        {children}
+      </body>
     </html>
   );
 }
--- app/not-found.tsx
+++ app/not-found.tsx
@@ -3,15 +3,15 @@ import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function NotFound() {
-	return (
-		<div className="flex h-screen flex-col items-center justify-center">
-			<h1 className="text-5xl font-bold">404</h1>
-			<p>페이지를 찾을 수 없습니다.</p>
-			<div className="mt-5">
-				<Button asChild>
-					<Link href="/">홈으로</Link>
-				</Button>
-			</div>
-		</div>
-	);
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      <h1 className="text-5xl font-bold">404</h1>
+      <p>페이지를 찾을 수 없습니다.</p>
+      <div className="mt-5">
+        <Button asChild>
+          <Link href="/">홈으로</Link>
+        </Button>
+      </div>
+    </div>
+  );
 }
