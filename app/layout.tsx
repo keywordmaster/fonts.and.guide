ad309baa1967e0dbe156bf30e8ec2f40e1502e17
Junglei Kim <67102441+imjlk@users.noreply.github.com>
2024-06-05 08:28:59 +0900

    Add dark mode (#1)
    * add provider and toggle for supporting dark mode
    
    * fix lint and formatting

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import Link from "next/link";
 import { gql } from "urql/core";
 
+import { ModeToggle } from "@/components/mode-toggle";
+import { ThemeProvider } from "@/components/theme-provider";
 import { GetRootLayoutQuery } from "@/gql/graphql";
 import { getClient } from "@/lib/urql/client";
 
@@ -42,31 +44,38 @@ export default async function RootLayout({
   );
 
   return (
-    <html lang="ko">
+    <html lang="ko" suppressHydrationWarning>
       <head>
         <title>{data?.generalSettings?.title}</title>
         <meta name="description" content={data?.generalSettings?.description} />
         <meta name="robots" content="noindex, nofollow" />
       </head>
       <body>
-        <header>
-          <div>
-            <h1>
-              <Link href="/">{data?.generalSettings?.title}</Link>
-            </h1>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <header className="flex justify-between">
+            <div>
+              <h1>
+                <Link href="/">{data?.generalSettings?.title}</Link>
+              </h1>
 
-            <h5>{data?.generalSettings?.description}</h5>
-          </div>
-
-          <ul>
-            {data?.primaryMenuItems?.nodes.map((node) => (
-              <li key={node.id}>
-                <Link href={node.uri}>{node.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </header>
-        {children}
+              <h5>{data?.generalSettings?.description}</h5>
+            </div>
+            <ul className="flex items-center">
+              {data?.primaryMenuItems?.nodes.map((node) => (
+                <li key={node.id}>
+                  <Link href={node.uri}>{node.label}</Link>
+                </li>
+              ))}
+              <ModeToggle />
+            </ul>
+          </header>
+          {children}
+        </ThemeProvider>
       </body>
     </html>
   );
