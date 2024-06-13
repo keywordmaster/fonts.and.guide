b7637db418796c2a5546ed1794363ba7fc2bf5b9
imjlk <i@m.junglei.kim>
2024-06-13 23:54:01 +0900

    feat: Add lazy loading for Google Analytics and Clarity scripts

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ export const runtime = "edge";
 
 import "./globals.css";
 
+import Script from "next/script";
 import { gql } from "urql/core";
 
 import Header from "@/components/layout/header";
@@ -51,6 +52,28 @@ export default async function RootLayout({
         <title>{data?.generalSettings?.title}</title>
         <meta name="description" content={data?.generalSettings?.description} />
         <meta name="robots" content="noindex, nofollow" />
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-K2NEKBNEHP"
+        />
+        <Script id="ga" strategy="lazyOnload">
+          {`
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+
+              gtag('config', 'G-K2NEKBNEHP');
+            `}
+        </Script>
+        <Script id="clarity" strategy="lazyOnload">
+          {`
+    (function(c,l,a,r,i,t,y){
+      c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+      t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+      y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+  })(window, document, "clarity", "script", "mrco71ybro");
+          `}
+        </Script>
       </head>
       <body className="">
         <ThemeProvider
