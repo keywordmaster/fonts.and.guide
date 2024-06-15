e21fa681b126707e7152072809bddb9cd00e7c56
Vespucci95 <donglonk@naver.com>
2024-06-15 18:11:45 +0900

    fix: base URL 재수정

--- utils/generate-breadcrumbs-schema.ts
+++ utils/generate-breadcrumbs-schema.ts
@@ -2,7 +2,7 @@ import { BreadcrumbItem } from "react-breadcrumbs-jsonld";
 
 const GenerateBreadcrumbsSchema = (
   pathMetaData: [string, string][],
-  baseURL = "https://example.com",
+  baseURL = process.env.NEXT_PUBLIC_URL,
 ): BreadcrumbItem[] => {
   return pathMetaData.map(([path, name]) => ({
     url: `${baseURL}${path}`,
