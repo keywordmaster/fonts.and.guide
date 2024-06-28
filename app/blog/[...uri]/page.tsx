1b2e8ae1d741c87cf58b530f92043c70fc0165af
imjlk <i@m.junglei.kim>
2024-06-28 23:11:34 +0900

    fix: fragment 제거

--- app/blog/[...uri]/page.tsx
+++ app/blog/[...uri]/page.tsx
@@ -6,7 +6,6 @@ export const runtime = "edge";
 
 import { gql } from "@urql/core";
 import { notFound } from "next/navigation";
-import { Fragment } from "react";
 
 import { GetPostQuery } from "@/gql/graphql";
 import { getClient } from "@/lib/urql/client";
@@ -60,7 +59,7 @@ export default async function Page({
   }
 
   return (
-    <Fragment>
+    <>
       <BreadcrumbsWithSchema
         pathMetaData={convertPostCategoryQueryToPathMetaData(data)}
         lastNodeType={LAST_NODE_TYPE.link}
