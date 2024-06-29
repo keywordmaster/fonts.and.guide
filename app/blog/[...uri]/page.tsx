898e563ad755a40886a2d41f49a13a214381d6ad
JuunKR <bluejun0510@naver.com>
2024-06-29 13:22:07 +0900

    blog page css 및 컴포넌트 추가

--- app/blog/[...uri]/page.tsx
+++ app/blog/[...uri]/page.tsx
@@ -2,6 +2,8 @@ import BreadcrumbsWithSchema, {
   LAST_NODE_TYPE,
 } from "@/components/layout/breadcrumbs-with-schema";
 
+import styles from "./page.module.css";
+
 export const runtime = "edge";
 
 import { gql } from "@urql/core";
@@ -64,22 +66,29 @@ export default async function Page({
 
   // TODO: 새로 추가된 컴포넌트의 레이아웃 재배치 필요
   return (
-    <>
-      <ScrollIndicator />
-      <BreadcrumbsWithSchema
-        pathMetaData={convertPostCategoryQueryToPathMetaData(data)}
-        lastNodeType={LAST_NODE_TYPE.link}
-      />
-      <h1>{data.post?.title}</h1>
-      <RedaingTime content={data.post?.content} />
-      <ToC content={data.post?.content} />
-      <div className="overflow-x-scroll p-4 bg-muted/50">
-        <pre>{JSON.stringify(data.breadcrumbs)}</pre>
+    <div className="relative">
+      <div className={`mr-64 ${styles.blogContent}`}>
+        <ScrollIndicator />
+        <BreadcrumbsWithSchema
+          pathMetaData={convertPostCategoryQueryToPathMetaData(data)}
+          lastNodeType={LAST_NODE_TYPE.link}
+        />
+        <h1>{data.post?.title}</h1>
+        <RedaingTime content={data.post?.content} />
+        <div className="overflow-x-scroll p-4 bg-muted/50">
+          <pre>{JSON.stringify(data.breadcrumbs)}</pre>
+        </div>
+        <article className="font-sans"
+          dangerouslySetInnerHTML={{ __html: data.post?.content }} />
       </div>
-      <article
-        className="font-sans"
-        dangerouslySetInnerHTML={{ __html: data.post?.content }}
-      />
-    </>
+      <div className="fixed top-0 right-0 w-64 h-screen flex flex-col pt-16">
+        <div className="flex-shrink-0">
+          <ScrollIndicator />
+        </div>
+        <div className="flex-grow overflow-y-auto">
+          <ToC content={data.post.content} />
+        </div>
+      </div>
+    </div>
   );
 }
