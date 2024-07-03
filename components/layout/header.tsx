2e2e3ff826fb27742ee4997ee62f0a6dda669c55
imjlk <i@m.junglei.kim>
2024-07-03 13:34:03 +0900

    fix

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -1,19 +1,28 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 interface Props {
   title: string;
 }
 
 const Header: React.FC<Props> = ({ title }) => {
+  const path = usePathname()
   return (
     <header className="sticky top-0 z-10 flex h-[57px] items-center gap-1 border-b bg-background px-4 justify-between">
       <div className="flex flex-col">
-        <Link
-          href="/"
-          prefetch
-          className="text-xl font-semibold"
-          dangerouslySetInnerHTML={{ __html: title }}
-        ></Link>
+        {path === "/" &&
+          <h1 className="text-xl font-semibold" dangerouslySetInnerHTML={{ __html: title }}></h1>
+        }
+        {path !== "/" &&
+          <Link
+            href="/"
+            prefetch
+            className="text-xl font-semibold"
+            dangerouslySetInnerHTML={{ __html: title }}
+          ></Link>
+        }
       </div>
     </header>
   );
