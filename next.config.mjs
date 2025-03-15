f8839f59d7ba2f6f7d03deb0063e2e931e97295e
imjlk <i@m.junglei.kim>
2025-03-15 21:44:06 +0900

    domain changed

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,27 @@
 import { setupDevPlatform } from "@cloudflare/next-on-pages/next-dev";
 
 if (process.env.NODE_ENV === "development") {
-  await setupDevPlatform();
+	await setupDevPlatform();
 }
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ["lucide-react"],
-  trailingSlash: true,
-  images: {
-    domains: ["wp.fonts.and.guide"],
-  },
-  // TODO: Redirect 설정이 필요할 때 활용
-  // redirects: async () => {
-  //   return [
-  //     {
-  //       source: '/blog/category/',
-  //       destination: '/blog/category/',
-  //       permanent: true,
-  //       statusCode: 301,
-  //     },
-  //   ]
-  // },
+	transpilePackages: ["lucide-react"],
+	trailingSlash: true,
+	images: {
+		domains: ["wp-fonts.and.guide"],
+	},
+	// TODO: Redirect 설정이 필요할 때 활용
+	// redirects: async () => {
+	//   return [
+	//     {
+	//       source: '/blog/category/',
+	//       destination: '/blog/category/',
+	//       permanent: true,
+	//       statusCode: 301,
+	//     },
+	//   ]
+	// },
 };
 
 export default nextConfig;
